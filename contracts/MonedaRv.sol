5cb686eab9a71ca9e11bb91125e96a62caf7881c
Rodrigo Villarreal <noiarek@gmail.com>
2018-12-06 01:53:49 -0600

    Implementing transfer function done

--- contracts/MonedaRv.sol
+++ contracts/MonedaRv.sol
@@ -9,10 +9,26 @@ contract MonedaRv {
 
 	mapping(address => uint256) public balanceOf;
 
+	event Transfer(
+		address indexed _from,
+		address indexed _to,
+		uint _value
+	);
+
 	constructor (uint256 _initialSupply) public {
 		//  Setting the initial supply to the person who deployed the contract
 		balanceOf[msg.sender] = _initialSupply;
 
 		totalSupply = _initialSupply;
-	}	
+	}
+
+	function transfer(address _to, uint256 _value) public returns (bool success) {
+		require(balanceOf[msg.sender] >= _value);
+		balanceOf[msg.sender] -= _value;
+		balanceOf[_to] = _value;
+		
+		Transfer(msg.sender, _to, _value);
+
+		return true;
+	}
 }
