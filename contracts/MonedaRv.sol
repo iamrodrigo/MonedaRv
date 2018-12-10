8576ba8ad004df95cbc78c63e22857a91ae12e42
Rodrigo Villarreal <noiarek@gmail.com>
2018-12-10 00:43:29 -0600

    Implementing approve and transfer from

--- contracts/MonedaRv.sol
+++ contracts/MonedaRv.sol
@@ -8,10 +8,19 @@ contract MonedaRv {
 	uint256 public totalSupply;
 
 	mapping(address => uint256) public balanceOf;
+	mapping(address => mapping (address => uint256)) public allowance;
+	
+
 
 	event Transfer(
 		address indexed _from,
 		address indexed _to,
+		uint _value
+	);
+
+	event Approval(
+		address indexed _owner,
+		address indexed _spender,
 		uint _value
 	);
 
@@ -25,10 +34,27 @@ contract MonedaRv {
 	function transfer(address _to, uint256 _value) public returns (bool success) {
 		require(balanceOf[msg.sender] >= _value);
 		balanceOf[msg.sender] -= _value;
-		balanceOf[_to] = _value;
+		balanceOf[_to] += _value;
 		
 		Transfer(msg.sender, _to, _value);
 
+		return true;
+	}
+
+	function approve(address _spender, uint256 _value) public returns (bool success) {
+		allowance[msg.sender][_spender] = _value;
+		Approval(msg.sender, _spender, _value);
+		return true;
+	}
+
+	function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+		require(balanceOf[_from] >= _value);
+		require(allowance[_from][msg.sender] >= _value);
+		balanceOf[_to] += _value;
+		balanceOf[_from] -= _value;
+		allowance[_from][msg.sender] -= _value;
+
+		Transfer(_from, _to, _value);
 		return true;
 	}
 }
